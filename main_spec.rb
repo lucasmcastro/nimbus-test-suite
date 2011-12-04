f64b3a1dc785acaa2067b3f178cdfe349e837b43
lucasmcastro <lucasmcastro@gmail.com>
2011-12-03 21:50:55 -0800

    Fixing main_spec to correctly add a client and check that.

--- main_spec.rb
+++ main_spec.rb
@@ -25,9 +25,8 @@ describe "virgin nimbus" do
     browser.h2.text.should == "2 DE 5 - CONFIGURAÇÃO DE REDE"
     browser.auto_fill :wizard_network
     browser.button(:text => "Próximo").click
-    browser.h2(:text => "3 DE 5 - CONFIGURAÇÃO DO OFFSITE").wait_until_present
+    browser.h2(:text => "3 de 5 - Configuração do Offsite").wait_until_present
     browser.button(:text => "Próximo").click
-    browser.h2(:text => "4 DE 5 - CONFIGURAÇÃO DE HORA").wait_until_present
     browser.h2.text.should == "4 DE 5 - CONFIGURAÇÃO DE HORA"
     browser.auto_fill :wizard_timezone
     browser.button(:text => "Próximo").click
@@ -46,12 +45,14 @@ describe "configured nimbus" do
 
   it "should add a client" do
     browser.h2.text.should == "BACKUPS" # dashboard
-    system("nimbusnotifier admin admin #{@nimbus_ip}")
+    nimbus_ip = browser.config['nimbus_ip']
+    system("nimbusnotifier admin admin #{nimbus_ip}")
     browser.link(:text => 'Computadores').click
     browser.link(:text => 'Ativar novo computador').click
     browser.link(:text => 'Editar').click
     browser.auto_fill :edit_client1
     browser.button(:text => 'Atualizar').click
     browser.link(:text => 'Ativar').click #TODO: ask team to change to protect this action with POST
+    browser.div(:id => 'computers').text.include?('Client 1').should == true
   end
 end
